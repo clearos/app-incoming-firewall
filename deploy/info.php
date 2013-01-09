54e619c1a5d7965cb0d4bd0fee44021b3ee98d01
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-01-09 17:29:26 +0000

    - Added to beta 1 build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'incoming_firewall';
-$app['version'] = '1.3.0';
+$app['version'] = '1.4.14';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
