e8b0b5c26659b4889de14e92f41748d7cc50aee3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:12:02 +0000

    Add new files and directories and commit changes to local repository

--- language/mk/incoming_firewall_lang.php
+++ language/mk/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = 'Домашни дојдовни конекции';
+$lang['incoming_firewall_app_description'] = 'стан на дојдовен Firewall држи на лошите момци со ограничување на пристапот на вашиот систем и ги блокира несаканите конекции.';
+$lang['incoming_firewall_app_name'] = 'дојдовни Firewall';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'Блокирани дојдовни конекции';
