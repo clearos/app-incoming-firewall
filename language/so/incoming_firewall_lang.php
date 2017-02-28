aa41af7c660e6a6021f08bf0b1aecb78debb51a9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:14:34 +0000

    Add new files and directories and commit changes to local repository

--- language/so/incoming_firewall_lang.php
+++ language/so/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = 'Oggol Connections socda';
+$lang['incoming_firewall_app_description'] = 'The app cusub Brannmur xajiya guys xun soo hawlaha si aad nidaamka iyo xanibeen xiriir la rabin.';
+$lang['incoming_firewall_app_name'] = 'Brannmur socda';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'Four Connections socda';
