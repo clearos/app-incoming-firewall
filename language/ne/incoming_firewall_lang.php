4001391f731e11530182e0a11890628059df2978
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:12:49 +0000

    Add new files and directories and commit changes to local repository

--- language/ne/incoming_firewall_lang.php
+++ language/ne/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = 'अनुमति आगमन जडान';
+$lang['incoming_firewall_app_description'] = 'आगमन फायरवल तपाईँको प्रणाली पहुँच सीमित र अनावश्यक जडानहरू अवरुद्ध गरेर खराब मान्छे बाहिर राख्छ।';
+$lang['incoming_firewall_app_name'] = 'आगमन फायरवल';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'अवरुद्ध आगमन जडान';
