07ced90ff1840d0409276dea43cd62b40aca6b26
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:05:45 +0000

    Add new files and directories and commit changes to local repository

--- language/sq/incoming_firewall_lang.php
+++ language/sq/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = 'Lejuara Lidhjet hyrëse';
+$lang['incoming_firewall_app_description'] = 'App Incoming Firewall mban liq jashtë duke kufizuar aksesin në sistemin tuaj dhe të bllokuar lidhjet padëshiruar.';
+$lang['incoming_firewall_app_name'] = 'Firewall Incoming';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'Bllokuar Lidhjet hyrëse';
