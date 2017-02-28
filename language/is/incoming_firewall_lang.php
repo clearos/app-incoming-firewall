194465e8a466b80a769ac528bee6e180ecd13d72
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:10:08 +0000

    Add new files and directories and commit changes to local repository

--- language/is/incoming_firewall_lang.php
+++ language/is/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = 'Leyfð komandi tengingar';
+$lang['incoming_firewall_app_description'] = 'The Komandi Firewall app heldur slæmur krakkar út með því að takmarka aðgang að tölvunni þinni og sljór óæskilegum tengingar.';
+$lang['incoming_firewall_app_name'] = 'Komandi Firewall';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'Læst komandi tengingar';
