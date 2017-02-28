8316072e0ca7c7bd95fabee2f50e6a0d01f8eee8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:08:13 +0000

    Add new files and directories and commit changes to local repository

--- language/et/incoming_firewall_lang.php
+++ language/et/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = 'Lubatud sissetulevad ühendused';
+$lang['incoming_firewall_app_description'] = 'Saabuva tulemüür app hoiab pahad välja, piirates juurdepääsu oma süsteemi ja blokeerib soovimatud ühendused.';
+$lang['incoming_firewall_app_name'] = 'Saabuva tulemüür';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'Blokeeritud sissetulevad ühendused';
