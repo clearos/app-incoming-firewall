51a80486bf70133545cd0db0a1968656a54be7ab
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:12:57 +0000

    Add new files and directories and commit changes to local repository

--- language/nl/incoming_firewall_lang.php
+++ language/nl/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = 'Toegestaan ​​Binnenkomende verbindingen';
+$lang['incoming_firewall_app_description'] = 'De inkomende Firewall app houdt de slechte kerels door het beperken van de toegang tot uw systeem en het blokkeren van ongewenste verbindingen.';
+$lang['incoming_firewall_app_name'] = 'inkomende Firewall';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'Geblokkeerde Binnenkomende verbindingen';
