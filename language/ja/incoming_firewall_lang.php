ff2600c2a8311a5311f9e993f3d8ba0bea0fe1d7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:10:24 +0000

    Add new files and directories and commit changes to local repository

--- language/ja/incoming_firewall_lang.php
+++ language/ja/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = '許可された接続';
+$lang['incoming_firewall_app_description'] = '着信ファイアウォールアプリは、あなたのシステムへのアクセスを制限し、不要な接続を遮断することにより、悪者を保持します。';
+$lang['incoming_firewall_app_name'] = '着信ファイアウォール';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'ブロックされた着信接続';
