a09b06431af8df541fb9e340bd272a540d871514
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:06:03 +0000

    Add new files and directories and commit changes to local repository

--- language/hy/incoming_firewall_lang.php
+++ language/hy/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = 'Թույլատրվում Ներգնա Connections';
+$lang['incoming_firewall_app_description'] = 'Մուտքային Firewall ծրագիրը շարունակում է վատ տղաներին դուրս սահմանափակելու մուտք գործել ձեր համակարգի եւ արգելափակման անցանկալի կապեր.';
+$lang['incoming_firewall_app_name'] = 'ներգնա Firewall';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'Արգելափակված Ներգնա Connections';
