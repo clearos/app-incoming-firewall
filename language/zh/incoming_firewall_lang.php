a96adf3c634c8f83e7fbb1d8934769594beb5d79
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:07:33 +0000

    Add new files and directories and commit changes to local repository

--- language/zh/incoming_firewall_lang.php
+++ language/zh/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = '允许传入的连接';
+$lang['incoming_firewall_app_description'] = '进入的防火墙应用程序通过限制访问你的系统，并阻止不需要的连接，保持了坏人。';
+$lang['incoming_firewall_app_name'] = '来电防火墙';
+$lang['incoming_firewall_blocked_incoming_connections'] = '阻止传入的连接';
