8229e2972ae12ddeeaf50aa220562f643fcff585
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:09:35 +0000

    Add new files and directories and commit changes to local repository

--- language/hu/incoming_firewall_lang.php
+++ language/hu/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = 'Engedélyezett Bejövő kapcsolatok';
+$lang['incoming_firewall_app_description'] = 'A bejövő tűzfal app tartja a rosszfiúkat való hozzáférés korlátozása révén a rendszer, és blokkolja a nem kívánt kapcsolatokat.';
+$lang['incoming_firewall_app_name'] = 'bejövő tűzfal';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'Blokkolt Bejövő kapcsolatok';
