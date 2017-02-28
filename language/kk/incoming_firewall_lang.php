bd593163be36abae7c45c36c52628ee6c45894e6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:10:40 +0000

    Add new files and directories and commit changes to local repository

--- language/kk/incoming_firewall_lang.php
+++ language/kk/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = 'Рұқсат Кіріс Қосылымдар';
+$lang['incoming_firewall_app_description'] = 'Кіріс Firewall Қолданбаға жүйеге қол жеткізуді шектеу және жағымсыз байланыстарын блоктау арқылы Жаман жігіт сақтайды.';
+$lang['incoming_firewall_app_name'] = 'кіріс Firewall';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'Бұғатталған Кіріс Қосылымдар';
