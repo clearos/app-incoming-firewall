6fb1cf15565f1e169d75810a517fb8bd4e677e8b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:13:54 +0000

    Add new files and directories and commit changes to local repository

--- language/ru/incoming_firewall_lang.php
+++ language/ru/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = 'Разрешенные входящие соединения';
+$lang['incoming_firewall_app_description'] = 'Приложение Входящий брандмауэр держит плохих парней, ограничивая доступ к вашей системе и блокирует нежелательные соединения.';
+$lang['incoming_firewall_app_name'] = 'Входящий брандмауэр';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'Заблокированные входящие соединения';
