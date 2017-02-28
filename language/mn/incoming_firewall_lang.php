4cea302ff39fb1f948ac2ade24b307e796e66382
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:12:17 +0000

    Add new files and directories and commit changes to local repository

--- language/mn/incoming_firewall_lang.php
+++ language/mn/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = 'Зөвшөөрөгдсөн ирж байгаа холболтууд';
+$lang['incoming_firewall_app_description'] = 'Ирж Firewall програм нь таны системд хандах хандалтыг хязгаарлах, хүсээгүй холболтыг хаах гэхэд муу залуус гарч хадгалж байдаг.';
+$lang['incoming_firewall_app_name'] = 'Ирж Firewall';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'Хааж ирж байгаа холболтууд';
