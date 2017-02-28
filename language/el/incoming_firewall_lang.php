da628f947bee6d2766c0560606aa8aab99d05946
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:08:54 +0000

    Add new files and directories and commit changes to local repository

--- language/el/incoming_firewall_lang.php
+++ language/el/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = 'Επιτρέπονται εισερχόμενες συνδέσεις';
+$lang['incoming_firewall_app_description'] = 'Η εφαρμογή Εισερχόμενη Τείχος προστασίας κρατά τους κακούς από περιορίζοντας την πρόσβαση στο σύστημά σας και να μπλοκάρει τις ανεπιθύμητες συνδέσεις.';
+$lang['incoming_firewall_app_name'] = 'εισερχόμενη Τείχος προστασίας';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'Αποκλεισμένοι εισερχόμενες συνδέσεις';
