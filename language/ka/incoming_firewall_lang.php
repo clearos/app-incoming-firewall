0170ad6da705d84d289d266dcafbde3fea1b0bc2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:10:32 +0000

    Add new files and directories and commit changes to local repository

--- language/ka/incoming_firewall_lang.php
+++ language/ka/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = 'დაშვებული შემომავალი კავშირები';
+$lang['incoming_firewall_app_description'] = 'შემომავალი Firewall app ინარჩუნებს ცუდები მიერ შეზღუდვის თქვენი სისტემის და ბლოკირება არასასურველი კავშირები.';
+$lang['incoming_firewall_app_name'] = 'შემომავალი Firewall';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'დაბლოკილი შემომავალი კავშირები';
