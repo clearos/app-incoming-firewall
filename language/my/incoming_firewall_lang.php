15ced9c85d022ffd843c5de38013dfc83bcb8a41
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:07:07 +0000

    Add new files and directories and commit changes to local repository

--- language/my/incoming_firewall_lang.php
+++ language/my/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed ဝင်လာသော Connections ကို';
+$lang['incoming_firewall_app_description'] = 'အဆိုပါအဝင် Firewall ကို app ကိုသင့် system မှ access ကိုကန့်သတ်ခြင်းနှင့်မလိုချင်တဲ့ဆက်သွယ်မှုများကိုပိတ်ဆို့ခြင်းအားဖြင့်မကောင်းတဲ့ယောက်ျားတွေထွက်စောင့်ရှောက်။';
+$lang['incoming_firewall_app_name'] = 'incoming Firewall ကို';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'ပိတ်ဆို့ဝင်လာသော Connections ကို';
