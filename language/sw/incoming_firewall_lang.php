66e0cce26f295c1ebd270cabc7b066e34b6f511d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:15:08 +0000

    Add new files and directories and commit changes to local repository

--- language/sw/incoming_firewall_lang.php
+++ language/sw/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = 'Kuruhusiwa Connections yaingia';
+$lang['incoming_firewall_app_description'] = 'programu yaingia Firewall anaendelea watu wabaya nje kwa kupunguza upatikanaji wa mfumo wako na kuzuia uhusiano zisizohitajika.';
+$lang['incoming_firewall_app_name'] = 'zinazoingia Firewall';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'Imefungwa Connections yaingia';
