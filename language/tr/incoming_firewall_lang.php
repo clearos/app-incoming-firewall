ada3b7bb143e840f91880e6a386b229e6fcd9cd7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 21:25:32 +0000

    Add new files and directories and commit changes to local repository

--- language/tr/incoming_firewall_lang.php
+++ language/tr/incoming_firewall_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['incoming_firewall_allowed_incoming_connections'] = 'Allowed Incoming Connections';
-$lang['incoming_firewall_app_description'] = 'The Incoming Firewall app keeps the bad guys out by limiting access to your system and blocking unwanted connections.';
-$lang['incoming_firewall_app_name'] = 'Incoming Firewall';
-$lang['incoming_firewall_blocked_incoming_connections'] = 'Blocked Incoming Connections';
+$lang['incoming_firewall_allowed_incoming_connections'] = 'İzin Gelen Bağlantılar';
+$lang['incoming_firewall_app_description'] = 'Gelen Firewall uygulaması sisteme erişimi kısıtlayarak ve istenmeyen bağlantıları engelleyerek kötü adamlar tutar.';
+$lang['incoming_firewall_app_name'] = 'gelen Güvenlik Duvarı';
+$lang['incoming_firewall_blocked_incoming_connections'] = 'Engellenen Gelen Bağlantılar';
